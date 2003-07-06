d1c0efcc569be9610ac85eb62a4d73d0ec96542a
Tim Ansell <tim@thousandparsec.net>
2003-07-06 08:31:38 +0000

    Now have a SDL mixin class and a bunch of wx stuff which uses it.

--- windows/starmap/winSDL.py
+++ windows/starmap/winSDL.py
@@ -2,12 +2,10 @@ import os
  
 from wxPython.wx import * 
 pygame = None
- 
-class wxSDLWindow(wxFrame): 
-	def __init__(self, parent, id, title = 'SDL window', **options): 
-		options['style'] = wxDEFAULT_FRAME_STYLE | wxTRANSPARENT_WINDOW 
-		wxFrame.__init__(*(self, parent, id, title), **options) 
 
+class wxSDLMixin:
+	def __init__(self):
+		self.__init = 0
 		self._surface = None
 
 		EVT_IDLE(self, self.OnIdle) 
@@ -18,53 +16,138 @@ class wxSDLWindow(wxFrame):
 			 self.Created()
 
 	def Created(self, evt):
-		# get the handle
-		print "created"
-		hwnd = self.GetHandle() 
+		if not self.__init:
+			# get the handle
+			print "created"
+			hwnd = self.GetHandle() 
 		
-		os.environ['SDL_WINDOWID'] = str(hwnd) 
-		if sys.platform == 'win32': 
-			os.environ['SDL_VIDEODRIVER'] = 'windib' 
+			os.environ['SDL_WINDOWID'] = str(hwnd) 
+			if sys.platform == 'win32': 
+				os.environ['SDL_VIDEODRIVER'] = 'windib' 
 
-		global pygame
-		import pygame as _pygame
-		pygame = _pygame
-		pygame.init()
-		pygame.display.init()
-		
-		EVT_SIZE(self, self.OnSize)
+			global pygame
+			import pygame as _pygame
+			pygame = _pygame
+			pygame.init()
+			pygame.display.init()
+	
+			EVT_SIZE(self, self.OnSize)
 
-		wxCallAfter(self.OnSize)
+			self.__init = 1
+
+			wxCallAfter(self.OnSize)
 		 
 	def OnIdle(self, ev): 
-		self.draw() 
+		self.Draw() 
  
 	def OnPaint(self, ev): 
-		self.draw()
+		self.Draw()
  
 	def OnSize(self, ev=None): 
-		x,y = self.GetSizeTuple() 
-		self._surface = pygame.display.set_mode((x,y)) 
+		if self.__init:
+			x,y = self.GetSizeTuple() 
+			self._surface = pygame.display.set_mode((x,y)) 
 
-		if ev:
-			ev.Skip() 
+			if ev:
+				ev.Skip() 
  
-	def draw(self): 
-		raise NotImplementedError('please define a .draw() method!') 
+	def Draw(self): 
+		raise NotImplementedError('please define a .Draw() method!') 
  
-	def getSurface(self):
+	def GetSurface(self):
 		return self._surface 
- 
- 
+
+class wxSDLWindow(wxSDLMixin, wxFrame): 
+	def __init__(self, parent, id, title = 'SDL window', **options): 
+		options['style'] = wxDEFAULT_FRAME_STYLE | wxTRANSPARENT_WINDOW 
+		wxFrame.__init__(*(self, parent, id, title), **options) 
+		
+		wxSDLMixin.__init__(self)
+
+class wxSDLScrolledWindow(wxScrolledWindow, wxSDLMixin): 
+	def __init__(self, parent, id, **options): 
+		options['style'] = wxDEFAULT_FRAME_STYLE | wxTRANSPARENT_WINDOW 
+		wxScrolledWindow.__init__(*(self, parent, id), **options) 
+
+		wxSDLMixin.__init__(self)
+
+class wxSDLPanel(wxSDLMixin, wxPanel):
+	def __init__(self, parent, id, **options): 
+		options['style'] = wxDEFAULT_FRAME_STYLE | wxTRANSPARENT_WINDOW 
+		wxPanel.__init__(*(self, parent, id), **options) 
+
+		wxSDLMixin.__init__(self)
+
+
+
+class CustomStatusBar(wxStatusBar):
+	def __init__(self, parent):
+		wxStatusBar.__init__(self, parent, -1)
+		self.SetFieldsCount(3)
+		self.sizeChanged = false
+		EVT_SIZE(self, self.OnSize)
+		EVT_IDLE(self, self.OnIdle)
+
+		self.SetStatusText("A Custom StatusBar...", 0)
+
+		self.cb = wxCheckBox(self, 1001, "toggle clock")
+		EVT_CHECKBOX(self, 1001, self.OnToggleClock)
+		self.cb.SetValue(true)
+
+		# set the initial position of the checkbox
+		self.Reposition()
+
+		# start our timer
+		self.timer = wxPyTimer(self.Notify)
+		self.timer.Start(1000)
+		self.Notify()
+
+	# Time-out handler
+	def Notify(self):
+		t = time.localtime(time.time())
+		st = time.strftime("%d-%b-%Y   %I:%M:%S", t)
+		self.SetStatusText(st, 2)
+
+	# the checkbox was clicked
+	def OnToggleClock(self, event):
+		if self.cb.GetValue():
+			self.timer.Start(1000)
+			self.Notify()
+		else:
+			self.timer.Stop()
+
+	def OnSize(self, evt):
+		self.Reposition()  # for normal size events
+
+		# Set a flag so the idle time handler will also do the repositioning.
+		# It is done this way to get around a buglet where GetFieldRect is not
+		# accurate during the EVT_SIZE resulting from a frame maximize.
+		self.sizeChanged = true
+
+	def OnIdle(self, evt):
+		if self.sizeChanged:
+			self.Reposition()
+
+	# reposition the checkbox
+	def Reposition(self):
+		rect = self.GetFieldRect(1)
+		self.cb.SetPosition(wxPoint(rect.x+2, rect.y+2))
+		self.cb.SetSize(wxSize(rect.width-4, rect.height-4))
+		self.sizeChanged = false
+
 if __name__ == "__main__": 
  
 	import time
  
  	ID_TIMER = 10123
-	class CircleWindow(wxSDLWindow): 
+
+	class CircleWindow(wxSDLScrolledWindow): 
 		"draw a circle in a wxPython / PyGame window" 
 		def __init__(self, parent, id, title = 'SDL window', **options):
-			wxSDLWindow.__init__(*(self, parent, id, title), **options)
+			wxSDLScrolledWindow.__init__(*(self, parent, id), **options)
+			
+			self.SetScrollbars(20, 20, 50, 50)
+			self.EnableScrolling(0,0)
 
 			self.topleft = [0,0]
 			self.tick = time.time()
@@ -72,9 +155,9 @@ if __name__ == "__main__":
 			self.timer = wxTimer(self, ID_TIMER)
 			EVT_TIMER(self, ID_TIMER, self.OnMove)
 			self.timer.Start(100)
-	
-		def draw(self):
-			surface = self.getSurface() 
+
+		def Draw(self):
+			surface = self.GetSurface() 
 			if not surface is None: 
 				topcolor = 5 
 				bottomcolor = 100 
@@ -87,12 +170,24 @@ if __name__ == "__main__":
 		def OnMove(self, ev): 
 			self.topleft[0] += 1
 			self.topleft[1] += 1
-			self.draw()
+			self.Draw()
+
+	class MyFrame(wxFrame):
+		def __init__(self, parent, ID, title, pos=wxDefaultPosition,
+						size=wxDefaultSize, style=wxDEFAULT_FRAME_STYLE):
+			wxFrame.__init__(self, parent, ID, title, pos, size, style)
+			panel = CircleWindow(self, -1)
 			
+			self.sb = CustomStatusBar(self)
+			self.SetStatusBar(self.sb)
+
+			tb = self.CreateToolBar(wxTB_HORIZONTAL|wxNO_BORDER|wxTB_FLAT|wxTB_TEXT)
+
 	def pygametest(): 
 		app = wxPySimpleApp() 
 		sizeT = (640,480) 
-		w = CircleWindow(None, -1, size = sizeT) 
+		#w = CircleWindow(None, -1, size = sizeT) 
+		w = MyFrame(None, -1, 'Testing', size = sizeT) 
 		w.Show(1) 
 		app.MainLoop() 
  
