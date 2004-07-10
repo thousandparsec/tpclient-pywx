5e22d0342814a17352af3f99271574ee866a7405
Tim Ansell <tim@thousandparsec.net>
2004-07-10 15:26:48 +0000

    Fixed more order stuff... Info window now has more information...

--- windows/winInfo.py
+++ windows/winInfo.py
@@ -4,6 +4,7 @@ displays all objects information.
 """
 
 import os
+from types import *
 
 # wxPython imports
 import wx
@@ -27,14 +28,17 @@ class winInfo(winBase):
 
 		self.title = wx.StaticText(self, -1, "No Object Selected.")
 		self.picture = wx.StaticBitmap(self, -1, wx.BitmapFromImage(wx.EmptyImage(128, 128)))
+		self.text = wx.TextCtrl(self, -1, "", style=wx.TE_MULTILINE|wx.TE_READONLY)
 
 		top = wx.BoxSizer(wx.VERTICAL)
 		top.Add(self.title, 0, wx.BOTTOM|wx.TOP|wx.ALIGN_CENTER, 3)
 
-		self.information = wx.GridSizer(3, 2, 0, 0)
+		information = wx.BoxSizer(wx.VERTICAL)
+		information.Add(self.text, 1, wx.EXPAND, 0)
+		
 		middle = wx.BoxSizer(wx.HORIZONTAL)
 		middle.Add(self.picture, 0, 0, 0)
-		middle.Add(self.information, 1, wx.EXPAND, 0)
+		middle.Add(information, 1, wx.EXPAND, 0)
 
 		top.Add(middle, 1, wx.EXPAND, 0)
 
@@ -77,4 +81,30 @@ class winInfo(winBase):
 			bitmap = wx.BitmapFromImage(wx.EmptyImage(128, 128))
 
 		self.picture.SetBitmap(bitmap)
+
+		# Add the object type specific information
+		s = ""
+		for key, value in object.__dict__.items():
+			if key.startswith("_") or key in \
+				('protocol', 'sequence', 'otype', 'length', 'order_types', 'order_number', 'contains'):
+				continue
+
+			if key == "ships":
+				s += "ships: "
+				for t, number in value:
+					s += "%s %s" % (number, ("Scouts, ", "Frigates, ", "Battleships, ")[t])
+				s = s[:-2] + "\n"
+				continue
+
+			if type(value) == StringType:
+				s += "%s: '%s'\n" % (key, value)
+			elif type(value) in (ListType, TupleType):
+				s += "%s: " % (key,)
+				for i in value:
+					s += "%s, " % (i,)
+				s = s[:-2] + "\n"
+			else:
+				s += "%s: %s\n" % (key, value)
+
+		self.text.SetValue(s)
 
