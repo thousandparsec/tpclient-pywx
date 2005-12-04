8c1e42b2019279ec533fecad282ab9e33b5647b0
Tim Ansell <tim@thousandparsec.net>
2005-12-04 03:05:54 +0000

    Fixed crazy CrossLine crap.

--- extra/wxFloatCanvas/CrossLine.py
+++ extra/wxFloatCanvas/CrossLine.py
@@ -10,20 +10,28 @@ except ImportError:
 t = array([[0,-1],[1,0]])
 
 class CrossLine(Line):
-	def __init__(self,Points,Number,
+	def __init__(self,start,delta,Number,
 				 LineColor = "Black",
 				 LineStyle = "Solid",
 				 LineWidth	= 1,
 				 InForeground = False):
 		self.Number = Number
 
-		Points = array(Points)
-		Points.resize((2+Number*4,2))
+		Points = array(shape=(Number*4,2))
+		Points[0] = start
+		Points[1] = array(start)+array(delta)
+		Points[2] = Points[3] = Points[1]
 
-		for i in range(0, Number*4, 4):
-			Points[i+4] = Points[i+1]
-			Points[i+5] = Points[i+1]+(Points[1]-Points[0])
-	
+		for i in range(4, len(Points), 4):
+			# i   = normal start
+			# i+1 = normal end
+			# i+2 = cross start
+			# i+3 = cross end
+			Points[i] = Points[i-3]
+			Points[i+1] = Points[i-3]+array(delta)
+
+			Points[i+2] = Points[i+3] = Points[i-3]
+
 		Line.__init__(self, Points, LineColor, LineStyle, LineWidth, InForeground)
 
 	def _Draw(self, dc, WorldToPixel, ScaleWorldToPixel, HTdc=None):
@@ -37,7 +45,7 @@ class CrossLine(Line):
 		for i in range(0, self.Number*4, 4):
 			Points[i+2] = (Points[i+1]+pslop*2.5).astype('i')
 			Points[i+3] = (Points[i+1]-pslop*2.5).astype('i')
-			
+
 		dc.SetPen(self.Pen)
 		dc.DrawLines(Points)
 		if HTdc and self.HitAble:
