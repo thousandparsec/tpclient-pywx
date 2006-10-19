d0efc0d85a5e2ea1ab8e5de00b52aa52ae5c88a4
Tim Ansell <tim@thousandparsec.net>
2006-10-19 13:30:58 +0000

    Changes to the setup.

--- setup.py
+++ setup.py
@@ -14,27 +14,6 @@ try:
 except ImportError:
 	pass
 
-if len(sys.argv) > 1 and sys.argv[1].startswith("--media"):
-	media_type = sys.argv[1].split('=')[-1].lower()
-	del sys.argv[1]
-else:
-	media_type = "small"
-
-try:
-	media_files = {}
-	for file in file("MEDIA-%s" % media_type.upper(), 'r').readlines():
-		file = file.strip()
-		dir = os.path.dirname(file)
-
-		if not media_files.has_key(dir):
-			media_files[dir] = []
-
-		media_files[dir].append(file)
-	media_files = media_files.items()
-except IOError:
-	print "Unable to find the required (%s) media list." % media_type
-	sys.exit()
-
 from __init__ import version
 version = ("%s.%s.%s" % version) + "-" + media_type
 
@@ -57,11 +36,10 @@ setup(
 		'extra',
 		'extra.wxFloatCanvas',
 		],
-	data_files=[(".",			("LICENSE", "COPYING", "README")),
+	data_files=((".",			("LICENSE", "COPYING", "README")),
 				("doc",			("doc/tips.txt",)),
 				("graphics",	glob.glob("graphics/*.png")),
-				("graphics",	glob.glob("graphics/*.ico")),
-	] + media_files,
+				("graphics",	glob.glob("graphics/*.ico"))),
 # Py2EXE stuff
 	windows=[{
 		"script": "tpclient-pywx",
