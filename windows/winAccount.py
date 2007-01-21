55f81f78e3b89f609e198cb89d453bccfb0a74c9
Tim Ansell <tim@thousandparsec.net>
2007-01-21 10:26:30 +0000

    WinAccount now uses XRC.

--- windows/winAccount.py
+++ windows/winAccount.py
@@ -12,224 +12,159 @@ import wx
 import wx.gizmos
 
 try:
-        from extra.GIFAnimationCtrl import GIFAnimationCtrl
+	from extra.GIFAnimationCtrl import GIFAnimationCtrl
 except ImportError:
-        from wx.animate import GIFAnimationCtrl
+	from wx.animate import GIFAnimationCtrl
 
 from tp.netlib import constants as features
 
 # Local Imports
-from winBase import winMainBase
-from utils import *
+from winBase import winMainBaseXRC
+from winConnect import usernameMixIn
+from xrc.winAccount import winAccountBase
 
-class winAccount(winMainBase):
+throbber = os.path.join("graphics", "downloading.gif")
+okay = os.path.join("graphics", "finished.gif")
+
+class winAccount(winAccountBase, winMainBaseXRC, usernameMixIn):
 	title = _("Account")
 
 	def __init__(self, application):
-		winMainBase.__init__(self, application)
-		panel = wx.Panel(self, -1)
+		winAccountBase.__init__(self, None)
+		winMainBaseXRC.__init__(self, application)	
+		usernameMixIn.__init__(self)
 
-		# The title
-		text_top = wx.StaticText( panel, -1, _("Create new Account"), style=wx.TE_CENTRE)
-		text_top.SetFont( wx.Font( 16, wx.ROMAN, wx.NORMAL, wx.BOLD ) )
+	def Show(self, show=True):
+		if not show:
+			return self.Hide()
 
-		# The fill in text areas
-		sizer_top = wx.BoxSizer( wx.HORIZONTAL )
-		sizer_top.Add( text_top, 1, wx.EXPAND, 5 )
+		self.Panel.Layout()
+		size = self.Panel.GetBestSize()
+		self.SetClientSize(size)
+		self.CenterOnScreen(wx.BOTH)
 
-		# Server
-		sizer_host = wx.BoxSizer( wx.HORIZONTAL )
+		url = self.application.gui.servers.URL.GetValue()
+		g = url.rfind('/')
+		url, game = url[:g], url[g+1:]
+		self.Server.SetValue(url)
 
-		text_host = wx.StaticText( panel, -1, _("Server"), style=wx.TE_RIGHT )
-		self.host = wx.TextCtrl( panel, -1, "" )
+		self.State("start")
 
-		# Throbber to display when waiting for connection to server
-		self.throbber = GIFAnimationCtrl( panel, -1)
-		import os
-		print os.getcwd()
-		self.throbber.LoadFile(os.path.join("graphics", "throbber.gif"))
-		self.throbber.Hide()
-
-		# Button to connect to the server
-		self.button_connect = wx.Button( panel, -1, "Check" )
-		self.button_connect.SetToolTip( wx.ToolTip("Check the server supports creating accounts.") )
-		self.button_connect.SetSize(self.throbber.GetBestSize())
-
-		sizer_host.Add(self.host,     1, wx.EXPAND)
-		sizer_host.Add(self.button_connect,  0, 0)
-		sizer_host.Add(self.throbber, 0, 0)
-		self.sizer_host = sizer_host
-
-		# Username
-		text_username = wx.StaticText( panel, -1, _("Username"), style=wx.TE_RIGHT )
-		self.username = wx.TextCtrl( panel, -1, "" )
-
-		# Password
-		text_password1 = wx.StaticText( panel, -1, _("Password"), style=wx.TE_RIGHT )
-		self.password1 = wx.TextCtrl( panel, -1, "", style=wx.TE_PASSWORD )
-		text_password2 = wx.StaticText( panel, -1, _(""), style=wx.TE_RIGHT )
-		self.password2 = wx.TextCtrl( panel, -1, "", style=wx.TE_PASSWORD )
-		self.password2.SetToolTip( wx.ToolTip("Retype the password.") )
-
-		text_email = wx.StaticText( panel, -1, _("Email"), style=wx.TE_RIGHT )
-		self.email = wx.TextCtrl( panel, -1, "" )
-
-		FLAGS = wx.ALIGN_RIGHT|wx.ALIGN_CENTER_VERTICAL|wx.EXPAND
-		grid = wx.FlexGridSizer( 0, 2, 5, 5 )
-		grid.AddGrowableCol(1)
-		grid.Add( text_host,      1, FLAGS )
-		grid.Add( sizer_host,     5, FLAGS )
-		grid.Add( text_username,  1, FLAGS )
-		grid.Add( self.username,  5, FLAGS )
-		grid.Add( text_password1, 1, FLAGS )
-		grid.Add( self.password1, 5, FLAGS )
-		grid.Add( text_password2, 1, FLAGS )
-		grid.Add( self.password2, 5, FLAGS )
-		grid.Add( text_email,     1, FLAGS )
-		grid.Add( self.email,     5, FLAGS )
-
-		# Buttons
-		self.button_ok     = wx.Button(panel, wx.ID_OK)
-		self.button_cancel = wx.Button(panel, wx.ID_CANCEL)
-		self.button_cancel.SetDefault()
-
-		buttons = wx.BoxSizer( wx.HORIZONTAL )
-		buttons.Add( self.button_ok, 0, wx.ALIGN_CENTRE)
-		buttons.Add( self.button_cancel, 0, wx.ALIGN_CENTRE)
-
-		# The main sizer
-		sizer = wx.BoxSizer( wx.VERTICAL )
-		sizer.Add( sizer_top, 0, wx.ALIGN_CENTRE|wx.EXPAND, 5 )
-		sizer.Add( grid,      1, wx.ALIGN_CENTRE|wx.EXPAND, 5 )
-		sizer.Add( buttons,   0, wx.ALIGN_CENTRE|wx.ALL, 5 )
-
-		# Join the panel and the base sizer
-		panel.SetAutoLayout( True )
-		panel.SetSizer( sizer )
-		sizer.Fit( panel )
-		sizer.SetSizeHints( self )
-
-		# Put the windows
-		self.SetSize(wx.Size(400, -1))
-		self.CenterOnScreen()
-
-		# Hook up the events
-		self.Bind(wx.EVT_BUTTON,   self.OnCheck,  self.button_connect)
-		self.Bind(wx.EVT_BUTTON,   self.OnOkay,   self.button_ok)
-		self.Bind(wx.EVT_BUTTON,   self.OnCancel, self.button_cancel)
-
-	def Show(self, a=True):
-		if a:
-			self.State("start")
-
-			url = self.application.gui.servers.URL.GetValue()
-			g = url.rfind('/')
-			url, game = url[:g], url[g+1:]
-
-			self.host.SetValue(url)
-		winMainBase.Show(self, a)
+		winMainBaseXRC.Show(self, show)
 
 	def State(self, mode):
 		if mode == "start":
 			# Enable the host entry box
-			self.host.Enable()
+			self.Server.Enable()
 
 			# Show and enable the connect button
-			self.button_connect.Show()
-			self.button_connect.Enable()
+			self.Check.Show()
+			self.Check.Enable()
 			
 			# Stop and hide the throbber
-			self.throbber.Stop()
-			self.throbber.Hide()
+			self.Checking.LoadFile(throbber)
+			self.Checking.Stop()
+			self.Checking.Hide()
 
 			# Disable the okay button
-			self.button_cancel.Enable()
-			self.button_ok.Disable()
+			self.Cancel.Enable()
+			self.Okay.Disable()
 
 			# Disable the entries	
-			self.username.Disable()
-			self.password1.Disable()
-			self.password2.Disable()
-			self.email.Disable()
+			self.Username.Disable()
+			self.GameShow.Disable()
+			self.Game.Disable()
+			self.Password1.Disable()
+			self.Password2.Disable()
+			self.Email.Disable()
 
-			# Re-layout everything
-			self.sizer_host.Layout()
+			self.Check.SetDefault()
+			self.Server.SetFocus()
 
 		if mode == "connecting":
 			# Stop the entry to host 
-			self.host.Disable()
+			self.Server.Disable()
 
 			# Hide the connect button
-			self.button_connect.Hide()
-			self.button_connect.Disable()
+			self.Check.Hide()
+			self.Check.Disable()
 
 			# Show and start the throbber
-			self.throbber.Show()
-			self.throbber.Play()
+			self.Checking.LoadFile(throbber)
+			self.Checking.Show()
+			self.Checking.Play()
 
 			# Disable the okay button
-			self.button_cancel.Enable()
-			self.button_ok.Disable()
+			self.Cancel.Enable()
+			self.Okay.Disable()
 
 			# Disable the entries	
-			self.username.Disable()
-			self.password1.Disable()
-			self.password2.Disable()
-			self.email.Disable()
-
-			self.sizer_host.Layout()
+			self.Username.Disable()
+			self.GameShow.Disable()
+			self.Game.Disable()
+			self.Password1.Disable()
+			self.Password2.Disable()
+			self.Email.Disable()
 
 		if mode == "details":
 			# Stop the entry to host
-			self.host.Disable()
+			self.Server.Disable()
 
 			# Show the connect button but disabled
-			self.button_connect.Show()
-			self.button_connect.Disable()
+			self.Check.Hide()
+			self.Check.Disable()
 
 			# Stop and hide the throbber
-			self.throbber.Stop()
-			self.throbber.Hide()
+			self.Checking.LoadFile(okay)
+			self.Checking.Show()
+			self.Checking.Play()
 
 			# Enable the okay button
-			self.button_cancel.Enable()
-			self.button_ok.Enable()
+			self.Cancel.Enable()
+			self.Okay.Enable()
 		
 			# Enable the entries	
-			self.username.Enable()
-			self.password1.Enable()
-			self.password2.Enable()
-			self.email.Enable()
-
-			self.sizer_host.Layout()
+			self.Username.Enable()
+			self.GameShow.Enable()
+			self.Game.Enable()
+			self.Password1.Enable()
+			self.Password2.Enable()
+			self.Email.Enable()
+			
+			self.Username.SetFocus()
+			self.Okay.SetDefault()
 
 		if mode == "saving":
 			# Stop the entry to host
-			self.host.Disable()
+			self.Server.Disable()
 
 			# Show the connect button but disabled
-			self.button_connect.Hide()
-			self.button_connect.Enable()
+			self.Check.Hide()
+			self.Check.Enable()
 
 			# Stop and hide the throbber
-			self.throbber.Show()
-			self.throbber.Play()
+			self.Checking.Show()
+			self.Checking.Play()
 
 			# Enable the okay button
-			self.button_ok.Disable()
-			self.button_cancel.Disable()
+			self.Okay.Disable()
+			self.Cancel.Disable()
 		
 			# Enable the entries	
-			self.username.Disable()
-			self.password1.Disable()
-			self.password2.Disable()
-			self.email.Disable()
+			self.Username.Disable()
+			self.GameShow.Disable()
+			self.Game.Disable()
+			self.Password1.Disable()
+			self.Password2.Disable()
+			self.Email.Disable()
 
-			self.sizer_host.Layout()
+		self.Panel.Layout()
+		size = self.Panel.GetBestSize()
+		self.SetClientSize(size)
 			
 	def OnCheck(self, evt):
 		self.State("connecting")
-		self.application.network.Call(self.application.network.Connect, self.host.GetValue(), 
+		self.application.network.Call(self.application.network.Connect, self.Server.GetValue(), 
 				debug=self.application.gui.connectto.config['debug'])
 
 	def OnNetworkConnect(self, evt):
@@ -250,16 +185,10 @@ class winAccount(winMainBase):
 		self.State("start")
 
 	def OnOkay(self, evt):
-		username = self.username.GetValue().strip().split('@')
-		if len(username) == 2:
-			username, game = username
-		else:
-			username = username[0]
-			game = ""
-
-		password1 = self.password1.GetValue().strip()
-		password2 = self.password2.GetValue().strip()
-		email = self.email.GetValue().strip()
+		username = self.GetUsername()
+		password1 = self.Password1.GetValue().strip()
+		password2 = self.Password2.GetValue().strip()
+		email = self.Email.GetValue().strip()
 
 		# Check the values are sensible
 		if username == "" or password1 == "" or password2 == "" or email == "":
@@ -271,14 +200,14 @@ class winAccount(winMainBase):
 			dlg.ShowModal()
 			return
 
-		url = self.host.GetValue()
-
+		url = self.Server.GetValue()
 		p = url.find('://')
 		if p != -1:
 			p += 3
 		else:
 			p = 0		
 
+		username, game = self.GetUsernameGame()
 		fullurl = "%s%s:%s@%s/%s" % (url[:p], username, password1, url[p:], game)
 		print fullurl
 		self.application.gui.connectto.ShowURL(fullurl)
---
+++ windows/xrc/winAccount.py
@@ -0,0 +1,72 @@
+# This file has been automatically generated.
+# Please do not edit it manually.
+
+# Python Imports
+import os.path
+
+# wxPython imports
+import wx
+from wx.xrc import XRCCTRL, XmlResourceWithHandlers
+
+class winAccountBase:
+	xrc = 'winAccount.xrc'
+
+	def PreCreate(self, pre):
+		""" This function is called during the class's initialization.
+		
+		Override it for custom setup before the window is created usually to
+		set additional window styles using SetWindowStyle() and SetExtraStyle()."""
+		pass
+
+	def __init__(self, parent, *args, **kw):
+		""" Pass an initialized wx.xrc.XmlResource into res """
+		f = os.path.join(os.path.dirname(__file__), self.xrc)
+		res = XmlResourceWithHandlers(f)		
+
+		# Figure out what Frame class (MDI, MiniFrame, etc) is actually our base...
+		bases = set()
+		def findbases(klass, set):
+			for base in klass.__bases__:
+				set.add(base)
+				findbases(base, set)
+		findbases(self.__class__, bases)
+
+		for base in bases:
+			if base.__name__.endswith("Frame"):
+				break
+		
+		# Two stage creation (see http://wiki.wxpython.org/index.cgi/TwoStageCreation)
+		pre = getattr(wx, "Pre%s" % base.__name__)()
+		res.LoadOnFrame(pre, parent, "winAccount")
+		self.PreCreate(pre)
+		self.PostCreate(pre)
+
+		# Define variables for the controls
+		self.Panel = XRCCTRL(self, "Panel")
+		self.Server = XRCCTRL(self, "Server")
+		self.Checking = XRCCTRL(self, "Checking")
+		self.Check = XRCCTRL(self, "Check")
+		if hasattr(self, "OnCheck"):
+			self.Bind(wx.EVT_BUTTON, self.OnCheck, self.Check)
+
+		self.Username = XRCCTRL(self, "Username")
+		self.GameShow = XRCCTRL(self, "GameShow")
+		if hasattr(self, "OnGameShow"):
+			self.Bind(wx.EVT_TOGGLEBUTTON, self.OnGameShow, self.GameShow)
+
+		self.GameTitle = XRCCTRL(self, "GameTitle")
+		self.Game = XRCCTRL(self, "Game")
+		self.Password1 = XRCCTRL(self, "Password1")
+		self.Password2 = XRCCTRL(self, "Password2")
+		self.Email = XRCCTRL(self, "Email")
+		self.ID_TEXTCTRL1 = XRCCTRL(self, "ID_TEXTCTRL1")
+		self.Okay = XRCCTRL(self, "wxID_OK")
+		if hasattr(self, "OnOkay"):
+			self.Bind(wx.EVT_BUTTON, self.OnOkay, self.Okay)
+
+		self.Cancel = XRCCTRL(self, "wxID_CANCEL")
+		if hasattr(self, "OnCancel"):
+			self.Bind(wx.EVT_BUTTON, self.OnCancel, self.Cancel)
+
+
+
