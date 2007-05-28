550465799a36d7fb45d51843a96b59b356f4020e
Tim Ansell <mithro@mithis.com>
2007-05-28 21:13:13 +0930

    Make the resource output a little bit nicer.
    (cherry picked from commit 22898ae2e633014697cd9ca756b723fe194bffac)

--- windows/winInfo.py
+++ windows/winInfo.py
@@ -105,6 +105,43 @@ class panelInformation(panelInformationBase):
 				s = s[:-2] + "\n"
 				continue
 
+			if key == "resources":
+				s += "Resources:\n"
+				for t, surface, minable, inaccess in value:
+					if surface+minable+inaccess == 0:
+						continue
+					if self.application.cache.resources.has_key(t):
+						res = self.application.cache.resources[t]
+						s+="\t"
+						if surface > 0:
+							if len(res.unit_singular) > 0:
+								s+="%s %s of %s on surface, " % (surface, \
+									[res.unit_singular, res.unit_plural][surface > 1],
+									[res.name_singular, res.name_plural][surface > 1])
+							else:
+								s+="%s %s on surface, " % (surface, [res.name_singular, res.name_plural][surface > 1])
+
+						if minable > 0:
+							if len(res.unit_singular) > 0:
+								s+="%s %s of %s minable, " % (minable, \
+									[res.unit_singular, res.unit_plural][minable > 1],
+									[res.name_singular, res.name_plural][minable > 1])
+							else:
+								s+="%s %s minable, " % (minable, [res.name_singular, res.name_plural][minable > 1])
+
+						if inaccess > 0:
+							if len(res.unit_singular) > 0:
+								s+="%s %s of %s inaccessible, " % (inaccess, \
+									[res.unit_singular, res.unit_plural][inaccess > 1],
+									[res.name_singular, res.name_plural][inaccess > 1])
+							else:
+								s+="%s %s inaccessible, " % (inaccess, [res.name_singular, res.name_plural][inaccess > 1])
+
+						s = s[:-2]+"\n"
+					else:
+						s+= "\tUnknown Resource %i, S: %i, M: %i, I: %s\n" % (t, surface, minable, inaccess)
+				continue
+
 			key = key.title()
 			if type(value) == StringType:
 				s += "%s: %s\n" % (key, value)
@@ -117,3 +154,4 @@ class panelInformation(panelInformationBase):
 				s += "%s: %s\n" % (key, value)
 
 		self.Details.SetValue(s)
+
