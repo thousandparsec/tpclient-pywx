27f1953a06af464df5c272cf1e3f1b1887151160
Tim Ansell <mithro@mithis.com>
2007-08-08 22:47:28 +0930

    Added the _str versions plus a _git version.

--- version.py
+++ version.py
@@ -1,15 +1,35 @@
 
 version = (0, 2, 99)
 
-import os.path
-if os.path.exists(".git"):
+# Add the git version if in a git tree...
+import os, os.path
+__path__ = os.path.realpath(os.path.dirname(__file__))
+installpath = __path__
+
+# Get the git version this tree is based on
+if os.path.exists(os.path.join(installpath, '.git')):
+	# Read in git's 'HEAD' file which points to the correct reff to look at
+	h = open(os.path.join(installpath, '.git', 'HEAD'))
+	# Read in the ref
+	ref = h.readline().strip().split(': ', 1)[1]
+	# This file has the SHA1
+	p = open(os.path.join(installpath, '.git', ref))
+
+	version_git = p.read().strip()
+
+	# What version are we trying to get too?
+	import time
 	if version[2] >= 99:
-		version_target = (version[0], version[1]+1, 0)
+		version_target = (version[0], version[1]+1, 0, time.strftime('%Y%m%d'))
 	else:
-		version_target = (version[0], version[1],   version[2]+1)
+		version_target = (version[0], version[1], version[2]+1, time.strftime('%Y%m%d'))
+
+	version_target_str = "%i.%i.%i.%s" % version_target
+
+version_str = "%i.%i.%i" % version[:3]
 
 if __name__ == "__main__":
 	if os.path.exists(".git"):
-		print '%i.%i.%i+%i.%i.%i' % (version+version_target)
+		print version_str+'+'+version_target_str, "(git %s)" % version_git
 	else:
-		print '%i.%i.%i' % version
+		print version_str
