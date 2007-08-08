b003ed32c4d2724025db11ad592dd12f94d08403
Tim Ansell <mithro@mithis.com>
2007-08-08 21:57:12 +0930

    Version file can now be run to get the current version.

--- version.py
+++ version.py
@@ -1,6 +1,15 @@
 
-version = (0, 3, 0)
-if version[2] == 0:
-	version_dev = (version[0], version[1]-1, 99)
-else:
-	version_dev = (version[0], version[1], version[2]-1)
+version = (0, 2, 99)
+
+import os.path
+if os.path.exists(".git"):
+	if version[2] >= 99:
+		version_target = (version[0], version[1]+1, 0)
+	else:
+		version_target = (version[0], version[1],   version[2]+1)
+
+if __name__ == "__main__":
+	if os.path.exists(".git"):
+		print '%i.%i.%i+%i.%i.%i' % (version+version_target)
+	else:
+		print '%i.%i.%i' % version
