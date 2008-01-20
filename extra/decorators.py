29ab629b69d1eee4586fe6f588f65dbefcd43dde
Tim Ansell <mithro@mithis.com>
2008-01-20 22:59:05 +1030

    Removed the excess prints.

--- extra/decorators.py
+++ extra/decorators.py
@@ -5,7 +5,6 @@ from tp.client.decorator import decorator
 def freeze_wrapper(func, self, *args, **kw):
 	try:
 		try:
-			print "Freezing", self
 			self.Freeze()
 		except AttributeError:
 			raise Warning("FreezeWrapper on %r but no Freeze method! (%s)" % (self, e))
@@ -14,7 +13,6 @@ def freeze_wrapper(func, self, *args, **kw):
 
 	finally:
 		try:
-			print "Thawing", self
 			self.Thaw()
 		except AttributeError, e:
 			raise Warning("FreezeWrapper on %r but no Thaw method! (%s)" % (self, e))
