cd3dd3bd167a6eaa3a418a72c984bca6d3b2f2de
Tim Ansell <mithro@mithis.com>
2008-02-13 23:46:26 +1030

    Added start of XRC for the order panels.

---
+++ windows/xrc/orderList.py
@@ -0,0 +1,57 @@
+# This file has been automatically generated.
+# Please do not edit it manually.
+
+# Python Imports
+import os.path
+
+# wxPython imports
+import wx
+from wx.xrc import XRCCTRL, XmlResourceWithHandlers
+
+# Local imports
+from requirements import location
+
+class BaseBase(wx.Panel):
+	xrc = os.path.join(location(), "windows", "xrc", 'orderList.xrc')
+
+	def PreCreate(self, pre):
+		""" This function is called during the class's initialization.
+		
+		Override it for custom setup before the window is created usually to
+		set additional window styles using SetWindowStyle() and SetExtraStyle()."""
+		pass
+
+	def __init__(self, parent, *args, **kw):
+		""" Pass an initialized wx.xrc.XmlResource into res """
+		f = os.path.join(os.path.dirname(__file__), self.xrc)
+		res = XmlResourceWithHandlers(f)		
+
+		# Two stage creation (see http://wiki.wxpython.org/index.cgi/TwoStageCreation)
+		pre = wx.PrePanel()
+		res.LoadOnPanel(pre, parent, "Base")
+		self.PreCreate(pre)
+		self.PostCreate(pre)
+
+		# Define variables for the controls
+		self.Choices = XRCCTRL(self, "Choices")
+		self.Type = XRCCTRL(self, "Type")
+		if hasattr(self, "OnType"):
+			self.Type.Bind(wx.EVT_CHOICE, self.OnType)
+
+		self.Number = XRCCTRL(self, "Number")
+		self.Add = XRCCTRL(self, "Add")
+		if hasattr(self, "OnAdd"):
+			self.Bind(wx.EVT_BUTTON, self.OnAdd, self.Add)
+
+		self.Delete = XRCCTRL(self, "Delete")
+		if hasattr(self, "OnDelete"):
+			self.Bind(wx.EVT_BUTTON, self.OnDelete, self.Delete)
+
+
+
+def strings():
+	pass
+	_("Choice 1");
+	_("Choice 2");
+	_("Add");
+	_("D");
